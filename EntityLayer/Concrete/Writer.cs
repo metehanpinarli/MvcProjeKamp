90bcd27f7f3740b0479c056c15ff3161c19ff865
MetehanPinarli <meteerkan1919@gmail.com>
2021-05-01 21:48:03 +0300

    sınırlamar tamam :key lenght

--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,27 @@ namespace EntityLayer.Concrete
 {
     public class Writer
     {
+        [Key]
         public int WriterID { get; set; }
+
+        [StringLength(50)]
         public string WriterName { get; set; }
+
+        [StringLength(50)]
         public string WriterSurName { get; set; }
+
+        [StringLength(100)]
         public string Writterİmage { get; set; }
+
+        [StringLength(50)]
         public string WritterMail { get; set; }
+
+        [StringLength(20)]
         public string WritterPassword { get; set; }
+
+
+        public ICollection<Heading> Headings { get; set; }
+        public ICollection<Content> Contents { get; set; }
 
     }
 }
