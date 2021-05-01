b837094f39b863046d84bc4530eafd34c2af69a2
MetehanPinarli <meteerkan1919@gmail.com>
2021-05-01 18:42:03 +0300

    KodeFirst Concrete Kısmı Kısmi Tamam

---
+++ EntityLayer/Concrete/Writer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Writer
+    {
+        public int WriterID { get; set; }
+        public string WriterName { get; set; }
+        public string WriterSurName { get; set; }
+        public string Writterİmage { get; set; }
+        public string WritterMail { get; set; }
+        public string WritterPassword { get; set; }
+
+    }
+}
